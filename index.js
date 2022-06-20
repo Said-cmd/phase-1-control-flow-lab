d09ff94227cf853e016a23713e7ec4fa5bd18fc6
Said Hussein <saidhussein12342@gmail.com>
2022-06-20 19:15:10 +0300

    commit tree 122f8f755367e4a71453ed2ae52df46741712e8f

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(Ride) {
+  if (Ride <= 400) {
+    return 'This one is on me!';
+  } else if (Ride > 1999 && Ride < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  } 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(City) {
+   return City === 'NYC'? 'Ok, sounds good.':'No go.';
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Tip) {
+  switch (Tip) {
+    case 'generous':
+        return 'Thank you so much.';
+    case 'not as generous':
+        return 'Thank you.';
+    default:
+        return 'Bye.';
+  }
 }
